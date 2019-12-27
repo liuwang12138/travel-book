051acde2b392dece0549b497faf803617fb071ca
liuwang <liuwang@ilarge.cn>
2019-12-27 15:58:35 +0800

    Hotel Manage Order All Pages

--- src/main/java/com/ustc/travelbook/po/HotelPO.java
+++ src/main/java/com/ustc/travelbook/po/HotelPO.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -20,18 +22,23 @@ public class HotelPO {
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
+    @NotBlank
     @TableField(value = "name")
     private String name;
 
+    @NotBlank
     @TableField(value = "location")
     private String location;
 
+    @NotNull
     @TableField(value = "price")
     private BigDecimal price;
 
+    @NotNull
     @TableField(value = "room_num")
     private Integer roomNum;
 
+    @NotNull
     @TableField(value = "available_num")
     private Integer availableNum;
 
